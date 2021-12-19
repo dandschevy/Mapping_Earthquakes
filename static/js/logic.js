813ce33dd834cb06c9c075f31a85d2a0d6edf87f
Shawna Chiaravalloti <90434559+dandschevy@users.noreply.github.com>
2021-12-18 16:39:54 -0800

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,15 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-
-// Create the map object with a center and zoom level.
-//let map = L.map("mapid", {
-    //center: [
-      //40.7, -94.5
-    //],
-    //zoom: 4
-  //});
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -22,14 +14,13 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// We create the tile layer that will be the background of our map.
-//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    //attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    //maxZoom: 18,
-    //id: 'mapbox/streets-v11',
-    //tileSize: 512,
-    //zoomOffset: -1,
-    //accessToken: API_KEY
-//});
-// Then we add our 'graymap' tile layer to the map.
-//streets.addTo(map);
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/dandschevy/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
+
